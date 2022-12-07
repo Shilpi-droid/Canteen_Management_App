aa361293205e9f3eee32df4b3af422485fe596be
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2022-12-08 01:21:05 +0530

    jeez 10

--- lib/ui/screens/wallet.dart
+++ lib/ui/screens/wallet.dart
@@ -16,6 +16,7 @@
 //   }
 // }
 
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 // import 'package:final_app/utils/colors.dart';
@@ -25,69 +26,83 @@ class WalletPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-     MaterialApp(
+    final deviceheight = MediaQuery.of(context).size.height;
+    final devicewidth = MediaQuery.of(context).size.width;
+    return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor:Color(0xff6D2113),
-            title: Text("Wallet",
-            style: TextStyle(
-              color: Colors.white,
-              
-            ),
-            textAlign: TextAlign.center,
-            ),
-            // backgroundColor: Colors.white,
-            //  Color(0xff6D2113),
-          ),
-          body: Padding(
-            padding: EdgeInsets.fromLTRB(20, 50, 20,20),
-            child: Column(
-              children:<Widget> [
-                 Container(
+        // appBar: AppBar(
+        //   backgroundColor:Color(0xff6D2113),
+        //   title: Text("Wallet",
+        //   style: TextStyle(
+        //     color: Colors.white,
+
+        //   ),
+        //   textAlign: TextAlign.center,
+        //   ),
+        //   // backgroundColor: Colors.white,
+        //   //  Color(0xff6D2113),
+        // ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
+          child: Column(
+            children: <Widget>[
+              Padding(padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+              child: Text(
+                "Balance",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 30),
+                ),
+                ),
+              Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
                   color: Color(0xff6D2113),
-                  
                 ),
-                height: 200,
+                height: deviceheight * .2,
+                width: devicewidth,
                 // width: 200,
-            
-                 child: Row(
-                    children:<Widget> [
-                 
-                      // Image(image: NetworkImage('https://s3-alpha-sig.figma.com/img/a816/a302/1441e7083f5d4e2e8c26b0260142c554?Expires=1671408000&Signature=Va-Pu9~gii4kSfj-~Gg-3ZqXdnBYenKE~EkkcdKI-gefgDSFc4W0Uv2sLoYpE5N5JUbwxm1rFB--Z4hoEVwcqaLx3Az0C8O4jF0L1mmTcWg3H~sw9Elj4b3h-FkjzjwUzcTtCQuShdGdn72anQva~3Tq7KFhlyDSjqLI6lONDzKbDDzIKZ7B3aVRpu8CF~mhA8o7caKSTkwOsC7rwQ-bpWx-kS0stHfwedEjmlHMy4YrFYOhhTus09KcwQ0KpuaA2GplXWB0wngkFaCWKgj1KujAoCAFVSEGIDs7QTmZJmF7wm2-aH0VuFW4upbcP7y54WnS~ppxtmASZ7HcDV9lsw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'),),
-                          // Image()
-                      Text("1000",
-                      textAlign:TextAlign.right,
+
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                        width: devicewidth * .05,
+                      ),
+                    Image(
+                      image: NetworkImage(
+                          'https://s3-alpha-sig.figma.com/img/a816/a302/1441e7083f5d4e2e8c26b0260142c554?Expires=1671408000&Signature=Va-Pu9~gii4kSfj-~Gg-3ZqXdnBYenKE~EkkcdKI-gefgDSFc4W0Uv2sLoYpE5N5JUbwxm1rFB--Z4hoEVwcqaLx3Az0C8O4jF0L1mmTcWg3H~sw9Elj4b3h-FkjzjwUzcTtCQuShdGdn72anQva~3Tq7KFhlyDSjqLI6lONDzKbDDzIKZ7B3aVRpu8CF~mhA8o7caKSTkwOsC7rwQ-bpWx-kS0stHfwedEjmlHMy4YrFYOhhTus09KcwQ0KpuaA2GplXWB0wngkFaCWKgj1KujAoCAFVSEGIDs7QTmZJmF7wm2-aH0VuFW4upbcP7y54WnS~ppxtmASZ7HcDV9lsw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA'),
+                      // fit: BoxFit.fill,
+                      height: deviceheight*.25,
+                      width: devicewidth*.25,
+                    ),
+                    SizedBox(
+                        width: devicewidth * .05,
+                      ),
+                    Text(
+                      
+                      "1000",
+                      textAlign: TextAlign.right,
                       style: TextStyle(
-                      //  fontsize:20,
-                     color: Colors.white,
-                   
-                       
+                        fontSize: 50,
+                        color: Colors.white,
                       ),
-                        
-                      ),
+                    ),
 
-
-                      // Text("Helllo"),
-                    ],
-                   ),
-                 ),
-            SizedBox(height: 20,),
-
-                 Text("Transtion History",
-                 textAlign: TextAlign.left,),
-               
-              ],
-             
-            ),
-              
-            ),
-          
+                    // Text("Helllo"),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Transtion History",
+                textAlign: TextAlign.left,
+              ),
+            ],
           ),
-       
-     
+        ),
+      ),
     );
   }
 }
